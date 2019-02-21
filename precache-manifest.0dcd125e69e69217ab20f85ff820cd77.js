08bbdbd7748db878322bbfdb63e251b886626732
sashasohrabi <shb.sohrabi@gmail.com>
2019-02-21 18:12:55 +0200

    Updates

---
+++ precache-manifest.0dcd125e69e69217ab20f85ff820cd77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "635860fd533afff61498",
+    "url": "/contact_manager/static/js/runtime~main.635860fd.js"
+  },
+  {
+    "revision": "febcd3d9389d70166567",
+    "url": "/contact_manager/static/js/main.febcd3d9.chunk.js"
+  },
+  {
+    "revision": "6f396972fe9ad9d15c10",
+    "url": "/contact_manager/static/js/1.6f396972.chunk.js"
+  },
+  {
+    "revision": "febcd3d9389d70166567",
+    "url": "/contact_manager/static/css/main.a5a46bc7.chunk.css"
+  },
+  {
+    "revision": "6f396972fe9ad9d15c10",
+    "url": "/contact_manager/static/css/1.5d7bbb92.chunk.css"
+  },
+  {
+    "revision": "5e5cd98a5057a445913162356711bf3f",
+    "url": "/contact_manager/index.html"
+  }
+];
