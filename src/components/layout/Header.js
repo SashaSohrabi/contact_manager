51f3a217d0d50e0cd53fe172c58ac6602e8b85f9
sashasohrabi <shb.sohrabi@gmail.com>
2019-02-21 20:18:02 +0200

    maburger menu.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -6,29 +6,38 @@ const Header = props => {
   const { branding } = props;
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-danger">
-      <div className="container">
-        <a href="/" className="navbar-brand">
-          {branding}
-        </a>
-        <div>
-          <ul className="navbar-nav">
-            <li className="navbar-item">
-              <Link to="/" className="nav-link">
-                <i className="fas fa-home" /> Home
-              </Link>
-            </li>
-            <li className="navbar-item">
-              <Link to="/contact/add" className="nav-link">
-                <i className="fas fa-plus" /> Add
-              </Link>
-            </li>
-            <li className="navbar-item">
-              <Link to="/about" className="nav-link">
-                <i className="fas fa-question" /> About
-              </Link>
-            </li>
-          </ul>
-        </div>
+      <a href="/" className="navbar-brand">
+        {branding}
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarText"
+        aria-controls="navbarText"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarText">
+        <ul className="navbar-nav">
+          <li className="navbar-item">
+            <Link to="/" className="nav-link">
+              <i className="fas fa-home" /> Home
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/contact/add" className="nav-link">
+              <i className="fas fa-plus" /> Add
+            </Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/about" className="nav-link">
+              <i className="fas fa-question" /> About
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
